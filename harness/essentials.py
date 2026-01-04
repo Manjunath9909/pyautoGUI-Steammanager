a40a3044c1ddcaec3839183e351161dc10f81707
Manjunath C <manjunathmc1996@gmail.com>
2026-01-04 23:20:01 +0530

    calling this done for now. it can launch games and let me know if games are not installed

--- harness/essentials.py
+++ harness/essentials.py
@@ -4,9 +4,9 @@ class position:
     x = 0
     y = 0
 
-def __init__(this, posx, posy):
-    this.x = posx
-    this.y = posy
+    def __init__(this, posx, posy):
+        this.x = posx
+        this.y = posy
 
 def isRunning(processName):
     for process in psutil.process_iter(['name']):
--- harness/launchGame.py
+++ harness/launchGame.py
@@ -9,31 +9,37 @@ def run(gameName):
         steamSearchBarPos = position(174, 178)
         steamSearchedFirstGamePos = position(204, 251)
         steamPlayButtonPos = position(582, 433)
-        foundGameRBG = (88, 111, 150)
+        foundGameRBG = (50, 58, 75)
         gameInstalled = (65, 194, 55)
         
         pgui.press('win')
         pgui.write('steam')
         pgui.press('enter')
-        time.sleep(2)
+        time.sleep(3)
         
-        pgui.move(steamSearchBarPos.x, steamSearchBarPos.y)
-        pgui.click()
-        pgui.write(gameName.lower())
+        pgui.moveTo(steamSearchBarPos.x, steamSearchBarPos.y)
         
-        pgui.move(steamSearchedFirstGamePos.x, steamSearchedFirstGamePos.y)
-        while True:
-            if pgui.pixelMatchesColor(steamSearchedFirstGamePos.x, steamSearchedFirstGamePos.y, foundGameRBG):
-                break
-            else:
-                print("Game does not exist")
-                return
+        print(pgui.position())
         pgui.click()
-        pgui.move(steamPlayButtonPos.x, steamPlayButtonPos.y)
-        if pgui.pixelMatchesColor(steamPlayButtonPos.x, steamPlayButtonPos.y, gameInstalled):
+        pgui.hotkey('ctrl', 'a')
+        pgui.press('backspace')
+        pgui.write(gameName)
+        
+        pgui.moveTo(steamSearchedFirstGamePos.x, steamSearchedFirstGamePos.y)
+        pgui.moveTo(steamSearchedFirstGamePos.x + 5, steamSearchedFirstGamePos.y)
+        if pgui.pixelMatchesColor(steamSearchedFirstGamePos.x, steamSearchedFirstGamePos.y, foundGameRBG):
+            print("Game found", end="\r", flush=True)
+        else:
+            print("Game does not exist")
+            return
+        
+        pgui.click()
+        pgui.moveTo(steamPlayButtonPos.x, steamPlayButtonPos.y)
+        if pgui.pixelMatchesColor(steamPlayButtonPos.x, steamPlayButtonPos.y, gameInstalled, tolerance=30):
+            
             pgui.click()
         else:
-            print("Game not installed on steam!")
+            print("Game not installed on steam!", end="\r", flush=True)
             exit(0)
     else:
         print("Game is already running!")
